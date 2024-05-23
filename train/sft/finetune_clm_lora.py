5ea5213f06c906e99aebd0e743e0df189b9e7afd
skyman <50543533@qq.com>
2024-05-23 17:06:52 +0800

    update finetune_clm_lora.py

--- train/sft/finetune_clm_lora.py
+++ train/sft/finetune_clm_lora.py
@@ -750,7 +750,8 @@ def main():
 
         metrics = trainer.evaluate()
 
-        max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+        max_eval_samples = data_args.max_eval_samples \
+            if data_args.max_eval_samples is not None else len(eval_dataset)
         metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
         try:
             perplexity = math.exp(metrics["eval_loss"])
