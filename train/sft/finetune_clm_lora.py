9962a5a95d96e34e2fa659916825e8d25081b5b7
skyman <50543533@qq.com>
2024-05-25 08:33:28 +0800

    update finetune_clm_lora.py

--- train/sft/finetune_clm_lora.py
+++ train/sft/finetune_clm_lora.py
@@ -261,8 +261,9 @@ class DataTrainingArguments:
         metadata={"help": "The number of processes to use for the preprocessing."},
     )
     keep_linebreaks: bool = field(
-        default=True, metadata={"help": "Whether to keep line breaks "
-                                        "when using TXT files or not."}
+        default=True,
+        metadata={"help": "Whether to keep line breaks "
+                          "when using TXT files or not."}
     )
 
     def __post_init__(self):
