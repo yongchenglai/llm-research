5d5c09fc0aebe09eb7cbb9b35b4cf002bec6133c
skyman <50543533@qq.com>
2024-06-01 22:11:35 +0800

    update gguf_api_test.py

---
+++ gguf_api_test.py
@@ -0,0 +1,18 @@
+# gguf_api_test.py
+# 基于requests进行服务测试
+
+import requests
+
+url = "http://localhost:8080/completion"
+headers = {"Content-Type": "application/json"}
+data = {
+    "prompt": "system\n你是一位智能助手!\nuser\n你好,请介绍一下埃隆马斯克。请用中文回答。\nassistant\n",
+    "n_predict": 128
+}
+response = requests.post(url, headers=headers, json=data)
+result = response.json()["content"]
+
+for line in result.split("\n"):
+    print(line)
+
+
