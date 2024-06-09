ad6c97c843e92464c928de9fe2d3aaf991abb9f2
skyman <50543533@qq.com>
2024-06-09 17:16:25 +0800

    update merge_peft_adapter.py

--- train/merge_peft_model/merge_muilt_peft_adapter.py
+++ train/merge_peft_model/merge_muilt_peft_adapter.py
@@ -4,7 +4,8 @@ from typing import Optional,List
 import peft
 import torch
 from peft import PeftConfig, PeftModel,PeftModelForSequenceClassification
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, HfArgumentParser,AutoModelForSequenceClassification
+from transformers import AutoConfig, AutoModelForCausalLM, \
+    AutoTokenizer, HfArgumentParser, AutoModelForSequenceClassification
 from peft.utils import _get_submodules
 
 @dataclass
@@ -13,8 +14,12 @@ class ScriptArguments:
     The name of the Casual LM model we wish to fine with PPO
     """
 
-    adapter_model_name: Optional[List[str]] = field(default=None, metadata={"help": "the model name"})
-    output_name: Optional[str] = field(default=None, metadata={"help": "the model name"})
+    adapter_model_name: Optional[List[str]] = field(
+        default=None,
+        metadata={"help": "the model name"})
+    output_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "the model name"})
 
 
 parser = HfArgumentParser(ScriptArguments)
@@ -22,14 +27,23 @@ script_args = parser.parse_args_into_dataclasses()[0]
 
 base_model = None
 for one_lora_path in script_args.adapter_model_name:
-    if base_model==None:
+    if base_model == None:
         peft_config = PeftConfig.from_pretrained(one_lora_path)
-        tokenizer = AutoTokenizer.from_pretrained(peft_config.base_model_name_or_path)
+        tokenizer = AutoTokenizer.from_pretrained(
+            peft_config.base_model_name_or_path)
         tokenizer.save_pretrained(f"{script_args.output_name}")
-        base_model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, return_dict=True, torch_dtype=torch.bfloat16)
+        base_model = AutoModelForCausalLM.from_pretrained(
+            peft_config.base_model_name_or_path,
+            return_dict=True,
+            torch_dtype=torch.bfloat16)
     peft_config = PeftConfig.from_pretrained(one_lora_path)
-    base_model = PeftModel.from_pretrained(base_model, one_lora_path,device_map={"": 0})
-    # model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, return_dict=True, device_map='auto',load_in_8bit=True)
+    base_model = PeftModel.from_pretrained(
+        base_model,
+        one_lora_path,
+        device_map={"": 0})
+
+    # model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path,
+    # return_dict=True, device_map='auto',load_in_8bit=True)
     # Load the Lora model
     base_model = base_model.merge_and_unload()
     base_model.eval()
