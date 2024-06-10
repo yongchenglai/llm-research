fc90e0263c03c1d3a4ae39a0fc1e04ca419fa45d
skyman <50543533@qq.com>
2024-06-10 19:19:51 +0800

    update quick_start.py

---
+++ vllm_openai_chat.py
@@ -0,0 +1,22 @@
+# vllm_openai_chat.py
+# Set OpenAI's API key and API base to use vLLM's API server.
+from openai import OpenAI
+
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:8000/v1"
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+chat_response = client.chat.completions.create(
+    model="Qwen/Qwen2-7B-Instruct-AWQ",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "说一下中国"},
+    ]
+)
+
+print("Chat response:", chat_response)
+
