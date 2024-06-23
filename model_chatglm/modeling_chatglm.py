39a53c42980869dfb3576d515456c8673660311f
skyman <50543533@qq.com>
2024-06-23 21:19:59 +0800

    update modeling_chatglm.py

--- model_chatglm/modeling_chatglm.py
+++ model_chatglm/modeling_chatglm.py
@@ -24,7 +24,8 @@ from transformers.modeling_outputs import (
 from transformers.modeling_utils import PreTrainedModel
 from transformers.utils import logging
 from transformers.generation.logits_process import LogitsProcessor
-from transformers.generation.utils import LogitsProcessorList, StoppingCriteriaList, GenerationConfig, ModelOutput
+from transformers.generation.utils import LogitsProcessorList, \
+    StoppingCriteriaList, GenerationConfig, ModelOutput
 
 from .configuration_chatglm import ChatGLMConfig
 
