81adfd9e859c05b403c91185ac95d8ade2d2d391
skyman <50543533@qq.com>
2024-07-15 18:49:42 +0800

    update bert_demo.py

---
+++ examples/bert_demo.py
@@ -0,0 +1,20 @@
+# bert_demo.py
+# bert_demo.py --model_name_or_path="google-bert/bert-base-uncased"
+from transformers import BertTokenizer, BertModel
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # "google-bert/bert-base-uncased"
+    parser.add_argument("--model_name_or_path", type=str, help='mode name or path')
+    args = parser.parse_args()
+
+    tokenizer = BertTokenizer.from_pretrained(args.model_name_or_path)
+    model = BertModel.from_pretrained(args.model_name_or_path)
+    print(model)
+    text = "Replace me by any text you'd like."
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+
+
