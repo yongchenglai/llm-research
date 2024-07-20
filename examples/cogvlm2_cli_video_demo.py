b645511cc305236e121c5d74acffb920bc6b3aab
skyman <50543533@qq.com>
2024-07-21 05:24:02 +0800

    update cogvlm2_cli_video_demo.py

--- examples/cogvlm2_cli_video_demo.py
+++ examples/cogvlm2_cli_video_demo.py
@@ -84,6 +84,8 @@ def main():
             trust_remote_code=True,
             quantization_config=BitsAndBytesConfig(
                 load_in_4bit=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_use_double_quant=True,
                 bnb_4bit_compute_dtype=TORCH_TYPE,
             ),
             low_cpu_mem_usage=True
