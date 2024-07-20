f7e685b51a5d6405589a3f1ac3697ffef74fe855
skyman <50543533@qq.com>
2024-07-20 09:08:06 +0800

    update model_baichuan.py

--- model/model_baichuan2/modeling_baichuan.py
+++ model/model_baichuan2/modeling_baichuan.py
@@ -707,11 +707,14 @@ class BaichuanForCausalLM(BaichuanPreTrainedModel):
             return_dict: Optional[bool] = None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
 
-        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_attentions = output_attentions \
+            if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+            output_hidden_states if output_hidden_states is not None
+            else self.config.output_hidden_states
         )
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        return_dict = return_dict \
+            if return_dict is not None else self.config.use_return_dict
 
         # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
         outputs = self.model(
