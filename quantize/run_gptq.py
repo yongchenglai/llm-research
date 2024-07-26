b2ee59caceba54f6859cd29533aede01b4f0d7f3
skyman <50543533@qq.com>
2024-07-26 17:07:25 +0800

    update  llama_autoawq.py

--- quantize/run_gptq.py
+++ quantize/run_gptq.py
@@ -11,32 +11,52 @@ from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 IGNORE_TOKEN_ID = LabelSmoother.ignore_index
 
 
+
+""" 
+preprocess函数接收一个包含对话数据的json列表作为输入,\n
+通过调用transformers库中的tokenizer对数据进行编码,\n
+并按照特定格式构建输入ID序列和目标ID序列.\n
+返回一个包含预处理数据的列表,这些数据已转换为PyTorch张量,适合于后续模型训练或推断
+"""
 def preprocess(
     sources,
     tokenizer: transformers.PreTrainedTokenizer,
     max_len: int,
     system_message: str = "You are a helpful assistant."
 ) -> Dict:
+    # roles字典：为对话中的角色("user"和"assistant")分配特殊的前缀标签，用于区分对话双方
     roles = {"user": "<|im_start|>user", "assistant": "<|im_start|>assistant"}
 
+    # im_start和im_end：指定tokenizer中im_start_id和im_end_id对应的整数ID。
     im_start = tokenizer.im_start_id
     im_end = tokenizer.im_end_id
+    # nl_tokens: 存储tokenizer处理换行符\n得到的输入ID序列。
     nl_tokens = tokenizer('\n').input_ids
+    # _system、_user和_assistant: 分别存储经过tokenizer处理后的"system"、
+    # "user"和"assistant"标签及其后的换行符对应的输入ID序列。
     _system = tokenizer('system').input_ids + nl_tokens
     _user = tokenizer('user').input_ids + nl_tokens
     _assistant = tokenizer('assistant').input_ids + nl_tokens
 
     # Apply prompt templates
+    # 定义空列表data, 用于存放预处理后的数据样本
     data = []
     # input_ids, targets = [], []
+    # 遍历输入数据sources中的每个样本(source)
     for i, source in enumerate(sources):
         source = source["conversations"]
+        # 检查首个对话是否由用户发起(即source[0]["from"]是否为"user")，
+        # 如果不是，则从源数据中移除首个对话。过滤无效的identity
         if roles[source[0]["from"]] != roles["user"]:
             source = source[1:]
 
+        # 初始化空列表input_id和target,分别用于存储当前样本的输入ID序列和目标ID序列
         input_id, target = [], []
+        # #添加系统消息: 将系统消息(包含system_message内容)转换为ID序列，添加到input_id和target中。
         system = [im_start] + _system + tokenizer(system_message).input_ids + [im_end] + nl_tokens
         input_id += system
+
+        # target中的非关键部分(如系统标签和消息内容)用IGNORE_TOKEN_ID填充。
         target += [im_start] + [IGNORE_TOKEN_ID] * (len(system)-3) + [im_end] + nl_tokens
         assert len(input_id) == len(target)
         for j, sentence in enumerate(source):
