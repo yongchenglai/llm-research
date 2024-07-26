20fa43b2c9fc1a65906c8d1c71ac950925be90dc
skyman <50543533@qq.com>
2024-07-26 22:26:20 +0800

    update  gptq_quantize.py

--- quantize/gptq_quantize.py
+++ quantize/gptq_quantize.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     print(f"quantization took: {end - start: .4f}s")
 
     # save quantized model
-    model.save_quantized(quant_path)
+    model.save_quantized(save_dir=quant_path)
 
     # save quantized model using safetensors
     model.save_quantized(quant_path, use_safetensors=True)
