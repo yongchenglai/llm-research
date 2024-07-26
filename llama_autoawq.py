87473f7e4ffacff77e24ed33eb6e679cfac9390a
skyman <50543533@qq.com>
2024-07-26 12:49:38 +0800

    update  modeling_minicpm.py

--- llama_autoawq.py
+++ llama_autoawq.py
@@ -3,41 +3,52 @@ from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
 from transformers import AwqConfig, AutoConfig
 from huggingface_hub import HfApi
-
-model_path = './FlagAlpha/Atom-7B-Chat/'
-quant_path = './FlagAlpha/Atom-7B-Chat-awq/'
+import argparse
 
 
-quant_config = {
-    "zero_point": True,
-    "q_group_size": 128,
-    "w_bit": 4,
-    "version": "GEMM"
-}
+if __name__ == "__main__":
 
-# Load model
-model = AutoAWQForCausalLM.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(
-    model_path,
-    trust_remote_code=True)
+    # llama_autoawq.py \
+    # --model_path='./FlagAlpha/Atom-7B-Chat/' \
+    # --quant_path='./FlagAlpha/Atom-7B-Chat-awq/'
+    parser = argparse.ArgumentParser(description='autoawq')
+    parser.add_argument("--model_path", type=str, help='model path')
+    parser.add_argument("--quant_path", type=str, help='quant path')
+    args = parser.parse_args()
 
-# Quantize
-model.quantize(tokenizer, quant_config=quant_config)
+    model_path = args.model_path
+    quant_path = args.quant_path
 
+    quant_config = {
+        "zero_point": True,
+        "q_group_size": 128,
+        "w_bit": 4,
+        "version": "GEMM"
+    }
 
-# modify the config file so that it is compatible with transformers integration
-quantization_config = AwqConfig(
-    bits=quant_config["w_bit"],
-    group_size=quant_config["q_group_size"],
-    zero_point=quant_config["zero_point"],
-    version=quant_config["version"].lower(),
-).to_dict()
+    # Load model
+    model = AutoAWQForCausalLM.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path,
+        trust_remote_code=True)
 
-# the pretrained transformers model is stored
-# in the model attribute + we need to pass a dict
-model.model.config.quantization_config = quantization_config
+    # Quantize
+    model.quantize(tokenizer, quant_config=quant_config)
 
-# save model weights
-model.save_quantized(quant_path)
-tokenizer.save_pretrained(quant_path)
+    # modify the config file so that it is compatible with transformers integration
+    quantization_config = AwqConfig(
+        bits=quant_config["w_bit"],
+        group_size=quant_config["q_group_size"],
+        zero_point=quant_config["zero_point"],
+        version=quant_config["version"].lower(),
+    ).to_dict()
+
+    # the pretrained transformers model is stored
+    # in the model attribute + we need to pass a dict
+    model.model.config.quantization_config = quantization_config
+
+    # save model weights
+    model.save_quantized(quant_path)
+    tokenizer.save_pretrained(quant_path)
+
 
