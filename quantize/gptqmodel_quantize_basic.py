15ac0bd4bd5b2bebb8e7eba624d0f3cfc1419bf5
skyman <50543533@qq.com>
2024-08-03 15:21:03 +0800

    update gradio_openai_chatbot_webserver.py

--- quantize/gptqmodel_quantize_basic.py
+++ quantize/gptqmodel_quantize_basic.py
@@ -10,7 +10,8 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(pretrained_model_id, use_fast=True)
     calibration_dataset = [
         tokenizer(
-            "gptqmodel is an easy-to-use model quantization library with user-friendly apis, based on GPTQ algorithm."
+            "gptqmodel is an easy-to-use model quantization library "
+            "with user-friendly apis, based on GPTQ algorithm."
         )
     ]
 
@@ -22,7 +23,8 @@ def main():
     # load un-quantized model, by default, the model will always be loaded into CPU memory
     model = GPTQModel.from_pretrained(pretrained_model_id, quantize_config)
 
-    # quantize model, the calibration_dataset should be list of dict whose keys can only be "input_ids" and "attention_mask"
+    # quantize model, the calibration_dataset should be list of dict
+    # whose keys can only be "input_ids" and "attention_mask"
     model.quantize(calibration_dataset)
 
     # save quantized model
@@ -33,14 +35,19 @@ def main():
     # or pass explcit token with: use_auth_token="hf_xxxxxxx"
     # (uncomment the following three lines to enable this feature)
     # repo_id = f"YourUserName/{quantized_model_dir}"
-    # commit_message = f"GPTQModel model for {pretrained_model_dir}: {quantize_config.bits}bits, gr{quantize_config.group_size}, desc_act={quantize_config.desc_act}"
+    # commit_message = f"GPTQModel model for {pretrained_model_dir}:
+    # {quantize_config.bits}bits, gr{quantize_config.group_size},
+    # desc_act={quantize_config.desc_act}"
     # model.push_to_hub(repo_id, commit_message=commit_message, use_auth_token=True)
 
     # alternatively you can save and push at the same time
     # (uncomment the following three lines to enable this feature)
     # repo_id = f"YourUserName/{quantized_model_dir}"
-    # commit_message = f"GPTQModel model for {pretrained_model_dir}: {quantize_config.bits}bits, gr{quantize_config.group_size}, desc_act={quantize_config.desc_act}"
-    # model.push_to_hub(repo_id, save_dir=quantized_model_dir, use_safetensors=True, commit_message=commit_message, use_auth_token=True)
+    # commit_message = f"GPTQModel model for {pretrained_model_dir}:
+    # {quantize_config.bits}bits, gr{quantize_config.group_size},
+    # desc_act={quantize_config.desc_act}"
+    # model.push_to_hub(repo_id, save_dir=quantized_model_dir, use_safetensors=True,
+    # commit_message=commit_message, use_auth_token=True)
 
     # save quantized model using safetensors
     model.save_quantized(quantized_model_id, use_safetensors=True)
@@ -55,7 +62,8 @@ def main():
     # model = GPTQModel.from_quantized(repo_id, device="cuda:0", use_safetensors=True,)
 
     # inference with model.generate
-    print(tokenizer.decode(model.generate(**tokenizer("gptqmodel is", return_tensors="pt").to(model.device))[0]))
+    print(tokenizer.decode(model.generate(**tokenizer(
+        "gptqmodel is", return_tensors="pt").to(model.device))[0]))
 
 
 if __name__ == "__main__":
