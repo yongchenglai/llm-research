629c364828a3a059376c9fad607f52d70cd722c5
skyman <50543533@qq.com>
2024-08-03 12:06:38 +0800

    update gradio_openai_chatbot_webserver.py

--- examples/gradio_openai_chatbot_webserver.py
+++ examples/gradio_openai_chatbot_webserver.py
@@ -31,6 +31,7 @@ def predict(message, history):
         messages=history_openai_format,  # Chat history
         temperature=args.temp,  # Temperature for text generation
         stream=True,  # Stream response
+        max_tokens=16384,
         extra_body={
             'repetition_penalty':
             1,
