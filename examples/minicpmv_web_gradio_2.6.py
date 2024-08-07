5042731e5d5095b878d747f6ac4c3049108a712c
skyman <50543533@qq.com>
2024-08-07 09:00:01 +0800

    update minicpmv_web_gradio_2.6.py

--- examples/minicpmv_web_gradio_2.6.py
+++ examples/minicpmv_web_gradio_2.6.py
@@ -124,7 +124,7 @@ def encode_image(image):
     # resize to max_size
     max_size = 448*16 
     if max(image.size) > max_size:
-        w,h = image.size
+        w, h = image.size
         if w > h:
             new_w = max_size
             new_h = int(h * max_size / w)
@@ -567,8 +567,11 @@ if __name__ == "__main__":
     assert device in ['cuda', 'mps']
 
     # Load model
-    # model_path = 'openbmb/MiniCPM-V-2_6'
     model_path = args.model_name_or_path
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path,
+        trust_remote_code=True)
+
     if 'int4' in model_path:
         if device == 'mps':
             print('Error: running int4 model with bitsandbytes on Mac is not supported right now.')
@@ -576,69 +579,84 @@ if __name__ == "__main__":
         model = AutoModel.from_pretrained(
             model_path,
             trust_remote_code=True)
-    else:
-        if args.multi_gpus:
-            from accelerate import load_checkpoint_and_dispatch, \
-                init_empty_weights, infer_auto_device_map
+    elif args.multi_gpus:
+        from accelerate import load_checkpoint_and_dispatch, \
+            init_empty_weights, infer_auto_device_map
 
-            with init_empty_weights():
-                model = AutoModel.from_pretrained(
-                    model_path,
-                    trust_remote_code=True,
-                    attn_implementation='sdpa',
-                    torch_dtype=torch.bfloat16,
-                )
-
-            device_map = infer_auto_device_map(
-                model,
-                max_memory={0: "10GB", 1: "10GB"},
-                no_split_module_classes=['SiglipVisionTransformer', 'Qwen2DecoderLayer'],
-            )
-
-            device_id = device_map["llm.model.embed_tokens"]
-            # first and last layer should be in same device
-            device_map["llm.lm_head"] = device_id
-            device_map["vpm"] = device_id
-            device_map["resampler"] = device_id
-            device_id2 = device_map["llm.model.layers.26"]
-            device_map["llm.model.layers.8"] = device_id2
-            device_map["llm.model.layers.9"] = device_id2
-            device_map["llm.model.layers.10"] = device_id2
-            device_map["llm.model.layers.11"] = device_id2
-            device_map["llm.model.layers.12"] = device_id2
-            device_map["llm.model.layers.13"] = device_id2
-            device_map["llm.model.layers.14"] = device_id2
-            device_map["llm.model.layers.15"] = device_id2
-            device_map["llm.model.layers.16"] = device_id2
-            # print(device_map)
-
-            model = load_checkpoint_and_dispatch(
-                model,
-                model_path,
-                dtype=torch.bfloat16,
-                device_map=device_map,
-            )
-        else:
+        with init_empty_weights():
             model = AutoModel.from_pretrained(
                 model_path,
-                device_map=device,
                 trust_remote_code=True,
+                attn_implementation='sdpa',
                 torch_dtype=torch.bfloat16,
-                attn_implementation="flash_attention_2",
-                quantization_config=BitsAndBytesConfig(
-                   load_in_4bit=True,
-                   bnb_4bit_quant_type="nf4",
-                   bnb_4bit_use_double_quant=True,
-                   bnb_4bit_compute_dtype=torch.bfloat16,
-                   # llm_int8_skip_modules=["out_proj", "kv_proj", "lm_head"],
-                ),
-                low_cpu_mem_usage=True,
             )
 
+        device_map = infer_auto_device_map(
+            model,
+            max_memory={0: "10GB", 1: "10GB"},
+            no_split_module_classes=['SiglipVisionTransformer', 'Qwen2DecoderLayer'],
+        )
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_path,
-        trust_remote_code=True)
+        device_id = device_map["llm.model.embed_tokens"]
+        # first and last layer should be in same device
+        device_map["llm.lm_head"] = device_id
+        device_map["vpm"] = device_id
+        device_map["resampler"] = device_id
+        device_id2 = device_map["llm.model.layers.26"]
+        device_map["llm.model.layers.8"] = device_id2
+        device_map["llm.model.layers.9"] = device_id2
+        device_map["llm.model.layers.10"] = device_id2
+        device_map["llm.model.layers.11"] = device_id2
+        device_map["llm.model.layers.12"] = device_id2
+        device_map["llm.model.layers.13"] = device_id2
+        device_map["llm.model.layers.14"] = device_id2
+        device_map["llm.model.layers.15"] = device_id2
+        device_map["llm.model.layers.16"] = device_id2
+        # print(device_map)
+
+        model = load_checkpoint_and_dispatch(
+            model,
+            model_path,
+            dtype=torch.bfloat16,
+            device_map=device_map,
+        )
+
+    elif args.quant == 4:
+        model = AutoModel.from_pretrained(
+            model_path,
+            device_map=device,
+            trust_remote_code=True,
+            torch_dtype=args.torch_dtype,
+            attn_implementation="flash_attention_2",
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_compute_dtype=args.torch_dtype,
+                # llm_int8_skip_modules=["out_proj", "kv_proj", "lm_head"],
+            ),
+            low_cpu_mem_usage=True,
+        )
+    elif args.quant == 8:
+        model = AutoModel.from_pretrained(
+            model_path,
+            device_map=device,
+            torch_dtype=args.torch_dtype,
+            trust_remote_code=True,
+            attn_implementation="flash_attention_2",
+            quantization_config=BitsAndBytesConfig(
+                load_in_8bit=True,
+                bnb_4bit_compute_dtype=args.torch_dtype,
+            ),
+            low_cpu_mem_usage=True
+        )
+    else:
+        model = AutoModel.from_pretrained(
+            model_path,
+            device_map=device,
+            torch_dtype=args.torch_dtype,
+            trust_remote_code=True
+        )
 
     model.eval()
     print(model)
