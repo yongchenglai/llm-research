9485b05f4830ed2a36dd67011290e73fc369320c
skyman <50543533@qq.com>
2024-08-07 08:54:14 +0800

    update minicpmv_web_gradio_2.5.py

--- examples/minicpmv_web_gradio_2.6.py
+++ examples/minicpmv_web_gradio_2.6.py
@@ -152,7 +152,7 @@ def encode_video(video):
         vr = VideoReader(video.file.path, ctx=cpu(0))
     sample_fps = round(vr.get_avg_fps() / 1)  # FPS
     frame_idx = [i for i in range(0, len(vr), sample_fps)]
-    if len(frame_idx)>MAX_NUM_FRAMES:
+    if len(frame_idx) > MAX_NUM_FRAMES:
         frame_idx = uniform_sample(frame_idx, MAX_NUM_FRAMES)
     video = vr.get_batch(frame_idx).asnumpy()
     video = [Image.fromarray(v.astype('uint8')) for v in video]
