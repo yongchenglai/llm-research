a71bf190f1f9565997e5691884e56a44c0fbba7f
skyman <50543533@qq.com>
2024-08-09 20:57:28 +0800

    update minicpmv_web_gradio_2.6.py

--- examples/minicpmv_web_gradio_2.6.py
+++ examples/minicpmv_web_gradio_2.6.py
@@ -144,8 +144,8 @@ def encode_image(image):
 def encode_video(video):
     def uniform_sample(frame_idx, max_num_frames):
         gap = len(frame_idx) / max_num_frames
-        idxs = [int(i * gap + gap / 2) for i in range(max_num_frames)]
-        return [frame_idx[i] for i in idxs]
+        frame_idxs = [int(i * gap + gap / 2) for i in range(max_num_frames)]
+        return [frame_idx[i] for i in frame_idxs]
 
     if hasattr(video, 'path'):
         vr = VideoReader(video.path, ctx=cpu(0))
