7938c5fc32a4e42f1a3a314194c0e4eb9623b092
skyman <50543533@qq.com>
2024-08-10 17:50:09 +0800

    update resampler.py

--- model/model_minicpmv2.6/resampler.py
+++ model/model_minicpmv2.6/resampler.py
@@ -173,9 +173,12 @@ class Resampler(nn.Module):
 
 
 class MultiheadAttention(nn.MultiheadAttention):
-    def __init__(self, embed_dim, num_heads, dropout=0., bias=True, add_bias_kv=False, 
-                 add_zero_attn=False, kdim=None, vdim=None, batch_first=False, device=None, dtype=None):
-        super().__init__(embed_dim, num_heads, dropout, bias, add_bias_kv, add_zero_attn, kdim, vdim, batch_first, device, dtype)
+    def __init__(self, embed_dim, num_heads, dropout=0.,
+                 bias=True, add_bias_kv=False,
+                 add_zero_attn=False, kdim=None, vdim=None,
+                 batch_first=False, device=None, dtype=None):
+        super().__init__(embed_dim, num_heads, dropout, bias, add_bias_kv,
+                         add_zero_attn, kdim, vdim, batch_first, device, dtype)
 
         # rewrite out_proj layer，with nn.Linear
         self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias, device=device, dtype=dtype)
