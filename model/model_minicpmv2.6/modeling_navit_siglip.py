618ab2d4e279417de865dd17cc3f5cf900abe723
skyman <50543533@qq.com>
2024-08-10 17:32:45 +0800

    update modeling_navit_siglip.py

--- model/model_minicpmv2.6/modeling_navit_siglip.py
+++ model/model_minicpmv2.6/modeling_navit_siglip.py
@@ -815,11 +815,14 @@ class SiglipEncoder(nn.Module):
             return_dict (`bool`, *optional*):
                 Whether or not to return a [`~utils.ModelOutput`] instead of a plain tuple.
         """
-        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_attentions = output_attentions \
+            if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+            output_hidden_states if output_hidden_states is not None
+            else self.config.output_hidden_states
         )
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        return_dict = return_dict \
+            if return_dict is not None else self.config.use_return_dict
 
         encoder_states = () if output_hidden_states else None
         all_attentions = () if output_attentions else None
