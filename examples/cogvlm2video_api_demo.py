8e8f9a8934900b4fe9d6fe1a21c4704b71d4fd04
skyman <50543533@qq.com>
2024-08-12 23:10:43 +0800

    update cogvlm2video_api_demo.py

--- examples/cogvlm2video_api_demo.py
+++ examples/cogvlm2video_api_demo.py
@@ -5,6 +5,10 @@ import torch
 from decord import cpu, VideoReader, bridge
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import argparse
+from flask import Flask, request, jsonify
+import traceback
+
+app = Flask(__name__)
 
 MODEL_PATH = "THUDM/cogvlm2-video-llama3-chat"
 
@@ -111,5 +115,46 @@ def predict(prompt, video_data, temperature):
         outputs = outputs[:, inputs['input_ids'].shape[1]:]
         response = tokenizer.decode(outputs[0], skip_special_tokens=True)
         return response
+
+
+@app.route('/video_qa', methods=['POST'])
+def video_qa():
+    if 'video' not in request.files:
+        return jsonify({'error': 'no video file found'}), 400
+
+    video = request.files['video']
+    if video.filename == '':
+        return jsonify({'error': 'no chosen file'}), 400
+
+    if 'question' not in request.form:
+        question = ""
+    else:
+        question = request.form['question']
+
+    if question is None or question == "" or question == "@Caption":
+        question = "Please describe the video in detail."
+
+    print("Get question:", question)
+
+    if 'temperature' not in request.form:
+        temperature = 0.001
+        print("No temperature found, use default value 0.001")
+    else:
+        temperature = float(request.form['temperature'])
+        print("Get temperature:", temperature)
+
+    try:
+        answer = predict(prompt=question,
+                         video_data=video.read(),
+                         temperature=temperature)
+        return jsonify(
+            {"answer": answer})
+    except:
+        traceback.print_exc()
+        return jsonify({"error": traceback.format_exc()}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host="0.0.0.0", port=5000)
 
 
