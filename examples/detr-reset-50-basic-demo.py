56f8826727829b33450a370a7959431447096566
skyman <50543533@qq.com>
2024-08-13 16:14:40 +0800

    update model_detr

--- examples/detr-reset-50-basic-demo.py
+++ examples/detr-reset-50-basic-demo.py
@@ -3,45 +3,57 @@ from transformers import DetrImageProcessor, DetrForObjectDetection
 import torch
 from PIL import Image
 import requests
-
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-model_path = "./facebook/detr-resnet-50"
+import argparse
 
 
-# you can specify the revision tag if you don't want the timm dependency
-processor = DetrImageProcessor.from_pretrained(
-    pretrained_model_name_or_path=model_path,
-    cache_dir="/home/xiaolai/meta-llama",
-    # local_files_only=True,
-    revision="no_timm",
-)
+if __name__ == "__main__":
 
-model = DetrForObjectDetection.from_pretrained(
-    pretrained_model_name_or_path=model_path,
-    cache_dir="/home/xiaolai/meta-llama",
-    # local_files_only=True,
-    revision="no_timm",
-)
+    parser = argparse.ArgumentParser(description='demo')
+    parser.add_argument("--model_name_or_path", type=str,
+                        default="facebook/detr-resnet-50")
+    parser.add_argument(
+        "--image_patch_or_url", type=str,
+        default="http://images.cocodataset.org/val2017/000000039769.jpg")
+    args = parser.parse_args()
 
-print(model)
+    image = Image.open(requests.get(args.image_patch_or_url, stream=True).raw)
+    model_path = args.model_name_or_path
 
-inputs = processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
+    # you can specify the revision tag if you don't want the timm dependency
+    processor = DetrImageProcessor.from_pretrained(
+        pretrained_model_name_or_path=model_path,
+        # cache_dir="/home/xiaolai/meta-llama",
+        # local_files_only=True,
+        revision="no_timm",
+    )
 
-# convert outputs (bounding boxes and class logits) to COCO API
-# let's only keep detections with score > 0.9
-target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(
-    outputs,
-    target_sizes=target_sizes,
-    threshold=0.9)[0]
+    model = DetrForObjectDetection.from_pretrained(
+        pretrained_model_name_or_path=model_path,
+        # cache_dir="/home/xiaolai/meta-llama",
+        # local_files_only=True,
+        revision="no_timm",
+    )
 
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    print(
+    print(model)
+
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(
+        outputs,
+        target_sizes=target_sizes,
+        threshold=0.9)[0]
+
+    for score, label, box in zip(results["scores"],
+                                 results["labels"],
+                                 results["boxes"]):
+        box = [round(i, 2) for i in box.tolist()]
+        print(
             f"Detected {model.config.id2label[label.item()]} with confidence "
             f"{round(score.item(), 3)} at location {box}"
-    )
+        )
 
 
