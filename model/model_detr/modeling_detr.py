3a07464f70f97c03a40f6c742d70550b4f851020
skyman <50543533@qq.com>
2024-08-13 14:25:49 +0800

    update model_detr

--- model/model_detr/modeling_detr.py
+++ model/model_detr/modeling_detr.py
@@ -775,7 +775,9 @@ class DetrDecoderLayer(nn.Module):
             output_attentions=output_attentions,
         )
 
-        hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
+        hidden_states = nn.functional.dropout(hidden_states,
+                                              p=self.dropout,
+                                              training=self.training)
         hidden_states = residual + hidden_states
         hidden_states = self.self_attn_layer_norm(hidden_states)
 
