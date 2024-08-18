c4df7f0576af60c2e7c709db81a27417b44a737d
skyman <50543533@qq.com>
2024-08-18 17:57:27 +0800

    update Dockerfile-parler-tts

--- examples/parler_tts_cli_demo.py
+++ examples/parler_tts_cli_demo.py
@@ -1,22 +1,31 @@
 # parler_tts_cli_demo.py
+"""
+docker exec -it parler-tts bash
+python3 /models/parler_tts_cli_demo.py
+"""
 import torch
 from parler_tts import ParlerTTSForConditionalGeneration
 from transformers import AutoTokenizer
 import soundfile as sf
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-large-v1").to(device)
-tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-large-v1")
+if __name__ == "__main__":
 
-prompt = "Hey, how are you doing today?"
-description = "A female speaker delivers a slightly expressive and animated speech with a moderate speed and pitch. The recording is of very high quality, with the speaker's voice sounding clear and very close up."
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model_name = "/models/parler-tts/parler-tts-large-v1"
 
-input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
-prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+    model = ParlerTTSForConditionalGeneration.from_pretrained(model_name).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    print(model)
 
-generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
-audio_arr = generation.cpu().numpy().squeeze()
-sf.write("parler_tts_out.wav", audio_arr, model.config.sampling_rate)
+    prompt = "Hey, how are you doing today?"
+    description = "A female speaker delivers a slightly expressive and animated speech with a moderate speed and pitch. The recording is of very high quality, with the speaker's voice sounding clear and very close up."
+
+    input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
+    prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+
+    generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
+    audio_arr = generation.cpu().numpy().squeeze()
+    sf.write("parler_tts_out.wav", audio_arr, model.config.sampling_rate)
 
 
