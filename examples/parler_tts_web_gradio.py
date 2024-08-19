c9fad90d88f94fa94ac30afd1127b161307c4b74
skyman <50543533@qq.com>
2024-08-19 09:23:08 +0800

    update parler_tts_web_demo.py

--- examples/parler_tts_web_gradio.py
+++ examples/parler_tts_web_gradio.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
         model = ParlerTTSForConditionalGeneration.from_pretrained(
             pretrained_model_name_or_path=args.model_name_or_path,
             # device_map=device,
-            trust_remote_code=True,
+            # trust_remote_code=True,
             torch_dtype=args.torch_dtype,
             attn_implementation="flash_attention_2",
             quantization_config=BitsAndBytesConfig(
@@ -122,7 +122,7 @@ if __name__ == "__main__":
             pretrained_model_name_or_path=args.model_name_or_path,
             # device_map=device,
             torch_dtype=args.torch_dtype,
-            trust_remote_code=True,
+            # trust_remote_code=True,
             attn_implementation="flash_attention_2",
             quantization_config=BitsAndBytesConfig(
                 load_in_8bit=True,
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         model = ParlerTTSForConditionalGeneration.from_pretrained(
             pretrained_model_name_or_path=args.model_name_or_path,
             # device_map=device,
-            # torch_dtype=args.torch_dtype,
+            torch_dtype=args.torch_dtype,
             # trust_remote_code=True
         )
 
