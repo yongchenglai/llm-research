34dcea1089a5c9c5af65e1cf5523260182f06ff4
skyman <50543533@qq.com>
2024-08-30 18:49:17 +0800

    update llama_chat_gradio.py

--- examples/minicpmv_web_gradio_2.6.py
+++ examples/minicpmv_web_gradio_2.6.py
@@ -701,8 +701,8 @@ if __name__ == "__main__":
         show_api=False,
         server_port=args.server_port,
         server_name=args.server_name,
-        ssl_certfile="cert.pem",
-        ssl_keyfile="key.pem",
-        ssl_verify=False,
+        # ssl_certfile="cert.pem",
+        # ssl_keyfile="key.pem",
+        # ssl_verify=False,
     )
 
