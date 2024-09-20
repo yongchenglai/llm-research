307f86c1fd083e2d948a5b53fd533b07b5336d9b
skyman <50543533@qq.com>
2024-09-20 21:58:38 +0800

    update vllm_based_chat_gradio.py

--- examples/vllm_based_chat_gradio.py
+++ examples/vllm_based_chat_gradio.py
@@ -5,42 +5,6 @@ from vllm import LLM, SamplingParams
 import torch
 from transformers import AutoTokenizer
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--model_path", type=str, default="openbmb/MiniCPM-1B-sft-bf16")
-parser.add_argument("--torch_dtype", type=str, default="bfloat16", choices=["float32", "bfloat16"])
-parser.add_argument("--server_name", type=str, default="127.0.0.1")
-parser.add_argument("--server_port", type=int, default=7860)
-parser.add_argument("--max_tokens", type=int, default=2048)
-# for MiniCPM-1B and MiniCPM-2B  model, max_tokens should be set to 2048
-
-args = parser.parse_args()
-
-# init model torch dtype
-torch_dtype = args.torch_dtype
-if torch_dtype == "" or torch_dtype == "bfloat16":
-    torch_dtype = torch.bfloat16
-elif torch_dtype == "float32":
-    torch_dtype = torch.float32
-elif torch_dtype == "float16":
-    torch_dtype = torch.float16
-else:
-    raise ValueError(f"Invalid torch dtype: {torch_dtype}")
-
-# init model and tokenizer
-path = args.model_path
-llm = LLM(
-    model=path,
-    tensor_parallel_size=1,
-    dtype=torch_dtype,
-    trust_remote_code=True,
-    gpu_memory_utilization=0.9,
-    max_model_len=args.max_tokens
-)
-tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-
-server_name = args.server_name
-server_port = args.server_port
-
 
 def vllm_gen(dialog: List, top_p: float, temperature: float, max_dec_len: int):
     """generate model output with huggingface api
@@ -190,11 +154,50 @@ with gr.Blocks(theme="soft") as demo:
         inputs=[chatbot],
         outputs=[chatbot])
 
-demo.queue()
-demo.launch(
-    server_name=server_name,
-    server_port=server_port,
-    show_error=True)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default="openbmb/MiniCPM-1B-sft-bf16")
+    parser.add_argument("--torch_dtype", type=str, default="bfloat16", choices=["float32", "bfloat16"])
+    parser.add_argument("--server_name", type=str, default="0.0.0.0")
+    parser.add_argument("--server_port", type=int, default=7860)
+    parser.add_argument("--max_tokens", type=int, default=2048)
+    # for MiniCPM-1B and MiniCPM-2B  model, max_tokens should be set to 2048
+
+    args = parser.parse_args()
+
+    # init model torch dtype
+    torch_dtype = args.torch_dtype
+    if torch_dtype == "" or torch_dtype == "bfloat16":
+        torch_dtype = torch.bfloat16
+    elif torch_dtype == "float32":
+        torch_dtype = torch.float32
+    elif torch_dtype == "float16":
+        torch_dtype = torch.float16
+    else:
+        raise ValueError(f"Invalid torch dtype: {torch_dtype}")
+
+    # init model and tokenizer
+    path = args.model_path
+    llm = LLM(
+        model=path,
+        tensor_parallel_size=1,
+        dtype=torch_dtype,
+        trust_remote_code=True,
+        gpu_memory_utilization=0.9,
+        max_model_len=args.max_tokens
+    )
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
+
+    server_name = args.server_name
+    server_port = args.server_port
+
+    demo.queue()
+    demo.launch(
+        server_name=server_name,
+        server_port=server_port,
+        show_error=True)
 
 
 
