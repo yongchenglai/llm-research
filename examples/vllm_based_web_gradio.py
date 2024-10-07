fabfb223ca7256bf6ec82423a70e53bcbf1c5422
skyman <50543533@qq.com>
2024-10-07 16:21:06 +0800

    update vllm_based_web_gradio.py

--- examples/vllm_based_web_gradio.py
+++ examples/vllm_based_web_gradio.py
@@ -12,7 +12,7 @@ def get_args():
     parser.add_argument("--model_path", type=str,
                         default="openbmb/MiniCPM-1B-sft-bf16")
     parser.add_argument("--torch_dtype", type=str, default="bfloat16",
-                        choices=["float32", "bfloat16"])
+                        choices=["float32", "bfloat16", "float16"])
     parser.add_argument("--server_name", type=str, default="0.0.0.0")
     parser.add_argument("--server_port", type=int, default=7860)
     # for MiniCPM-1B and MiniCPM-2B  model, max_tokens should be set to 2048
